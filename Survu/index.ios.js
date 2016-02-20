c1b3446c27f2b1e39413d59ff68597ae3ded7261
joanna-chen <joannachen.x@gmail.com>
2016-02-20 05:07:45 -0500

    working MVP

--- Survu/index.ios.js
+++ Survu/index.ios.js
@@ -1,5 +1,10 @@
 const styles = require('./styles.js')
 const Firebase = require('firebase');
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const { ListView } = React;
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -17,19 +22,47 @@ class Survu extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          style={styles.listview}/>
+
+        <ActionButton title="Add" onPress={() => {}} />
+
       </View>
     );
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+  }
+
+  _renderItem(item) {
+    return (
+      <ListItem item={item} onPress={() => {}} />
+    );
+  }
+
+
+
+
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
+    })
+  }
 }
+
+
+
 
 AppRegistry.registerComponent('Survu', () => Survu);
