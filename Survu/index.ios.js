cf9a8b9f9710c8ab7c736e021c4eb38920e0ca74
joanna-chen <joannachen.x@gmail.com>
2016-02-20 10:39:31 -0500

    survey componenets

--- Survu/index.ios.js
+++ Survu/index.ios.js
@@ -3,7 +3,7 @@ const Firebase = require('firebase');
 const StatusBar = require('./components/StatusBar');
 const ActionButton = require('./components/ActionButton');
 const ListItem = require('./components/ListItem');
-const Form = require('./components/form')
+//const Form = require('./components/form');
 const { ListView, TextInput, Switch, SliderIOS, AlertIOS, DatePickerIOS, Picker, PickerIOS } = React;
 
 /**
@@ -20,7 +20,11 @@ import React, {
 } from 'react-native';
 
 const FirebaseUrl = 'https://amber-inferno-9686.firebaseio.com/data';
+const resultUrl = 'https://amber-inferno-9686.firebaseio.com/result';
 var ref = new Firebase(FirebaseUrl);
+var ref_results = new Firebase(resultUrl);
+
+var arr = [];
 
 class Survu extends Component {
   render() {
@@ -34,7 +38,33 @@ class Survu extends Component {
           renderRow={this._renderItem.bind(this)}
           style={styles.listview}/>
 
-          <ActionButton title="Add" onPress={this._newSurvey.bind(this)} />
+        <ActionButton title="Add" onPress={this._newSurvey.bind(this)} />
+
+        <Switch
+          ref='switch1'
+          onValueChange={(value) => this.setState({falseSwitchIsOn: value})}
+          style={{marginBottom: 50}}
+          value={this.state.falseSwitchIsOn}
+          />
+
+        <Switch
+          ref='switch2'
+          onValueChange={(value) => this.setState({trueSwitchIsOn: value})}
+          value={this.state.trueSwitchIsOn} />
+
+        <Text style={styles.text} >
+          {'Slider: ' + this.state.value}
+        </Text>
+        <SliderIOS
+          ref='slider'
+          {...this.props}
+          onValueChange={(value) => this.setState({value: value})}
+          minimumValue={0}
+          maximumValue={10}
+          step={1}/>
+
+
+        <ActionButton title="Done" onPress={this._submitSurvey.bind(this)} />
 
       </View>
     );
@@ -48,6 +78,12 @@ class Survu extends Component {
         rowHasChanged: (row1, row2) => row1 !== row2,
       })
     };
+  }
+
+  _submitSurvey() {
+    arr.push(switch1.value,switch2.value,slider.value);
+    ref_results.push(arr);
+    //will do later
   }
 
   _newSurvey() {
